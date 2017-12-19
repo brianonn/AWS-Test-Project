9739a19ca5f6d2a99b81ec6a00bec0d601d57c79
Jason Kiraly <jaykiraly@gmail.com>
2017-12-19 10:50:30 -0800

    Changed JSON return to html.

--- index.py
+++ index.py
@@ -7,10 +7,21 @@ def handler(event, context):
         'output': 'Hello JASON',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
+    html = """
+        <html>
+            <head>AWS-Test Project</head>
+            <body>
+                <div class="jason-test-div">
+                    Hello Jason/Brian.
+                </div>
+            </body>
+        </html>
+    """
     return {'statusCode': 200,
-            'body': json.dumps(data),
+            'body': html
             'headers': {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST'
-            }}
+            }
+        }
